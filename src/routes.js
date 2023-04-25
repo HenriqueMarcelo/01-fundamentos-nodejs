9d8f2b5326cf65e9bbf4fc5ab5c176f6618cb4a4
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-04-25 14:53:06 -0300

    feat: remove user

--- src/routes.js
+++ src/routes.js
@@ -37,9 +37,11 @@ export const routes = [
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
         handler: (req, res) => {
-            return res
-                .writeHead(202)
-                .end()
+            const {id} = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         }
     }
 ]
